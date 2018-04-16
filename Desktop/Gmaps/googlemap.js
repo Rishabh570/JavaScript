dd162d489fdd018bc6db5fe660e6b637bdfd691c
Ricky <www.rishabhrawat570@gmail.com>
2018-04-16 16:49:40 +0530

    Add weather report UI and logic.

--- Desktop/Gmaps/googlemap.js
+++ Desktop/Gmaps/googlemap.js
@@ -10,7 +10,7 @@ function initMap() {
                 lat : position.coords.latitude,
                 lng : position.coords.longitude
             };
-            console.log(pos);
+            console.log(pos.lat, pos.lng);
             map.setCenter(pos);
         });
         var marker = new google.maps.Marker({
@@ -24,12 +24,27 @@ function initMap() {
     }
 
     google.maps.event.addListener(marker, 'dragend', function(evt){
-        document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(3) + ' Current Lng: ' + evt.latLng.lng().toFixed(3) + '</p>';
-    });
+        var latitude = evt.latLng.lat().toFixed(3);
+        var longitude = evt.latLng.lng().toFixed(3);
+        document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + latitude + ' Current Lng: ' + longitude + '</p>';
+
+        // FETCHING THE CURRENT WEATHER
+        let url = ("https://api.darksky.net/forecast/a1e2c034cc0988736d3da59909cc21f6/" + latitude + "," + longitude );
+        $.ajax({
+            url : url,
+            dataType : 'JSONP',
+            success : function (response) {
+                var result = response.currently.summary;
+                console.log(result);
+                // console.log(result)
+                $('.weather').html(result);
+            }
+        });
 
     google.maps.event.addListener(marker, 'dragstart', function(evt){
         document.getElementById('current').innerHTML = '<p>Currently dragging marker...</p>';
     });
+});
 }
 
 // var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
